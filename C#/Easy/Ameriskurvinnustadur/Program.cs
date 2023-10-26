cbed7bc0f8b0aa53bd26181c7e285795c330af16
Colby Bailey <83721925+bagoferasers@users.noreply.github.com>
2023-10-26 11:13:06 -0600

    Completed 2 Easy Problems
    aha ha ha ahaaaa

---
+++ C#/Easy/Ameriskurvinnustadur/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/// <summary>
+/// Kattis Problem
+/// Author: Colby Bailey
+/// Date: 26 October, 2023
+/// </summary>
+public class Ameriskurvinnustadur
+{
+    /// <summary>
+    /// Main takes in an integer and multiplies it by the length of the football
+    /// field. It then returns the double, lol. Werk.
+    /// </summary>
+    public static void Main( )
+    {
+        double field = 0.09144d;
+        int inputNumber = Convert.ToInt32( Console.ReadLine( ) );
+        Console.WriteLine( ( field * inputNumber ) );
+    }
+}
---
+++ C#/Easy/Hradgreining/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+/// <summary>
+/// Kattis Problem
+/// Author: Colby Bailey
+/// Date: 26 October, 2023
+/// </summary>
+public class Hradgreining 
+{
+    /// <summary>
+    /// Main function that takes in a dna string of at least three characters
+    /// and checks to see if it contains the sequence 'COV.'
+    /// </summary>
+    /// <param name="args">Not used.</param>
+    public static void Main( string[] args )
+    {
+        string? line = Console.ReadLine( );
+        if( line == null )
+        {
+            Console.WriteLine( "Null!" );
+            Environment.Exit( 0 );
+        }
+        else if( line == "" )
+        {
+            Console.WriteLine( "Blank!" );
+            Environment.Exit( 0 );
+        }
+        else if( line.Length < 3 )
+        {
+            Console.WriteLine( "Less than three letters!" );
+            Environment.Exit( 0 );
+        }
+        int i = 0, j = 1, k = 2;
+
+        /// <summary>
+        /// Checks for sequence 'COV'
+        /// </summary>
+        /// <returns>Returns 'Veikur!' if 'COV' sequence is found. Returns
+        /// 'Ekki veikur!' if 'COV' sequence is not found.</returns>
+        while( k <= ( line.Length - 1 ) )
+        {
+            if( 
+                char.ToUpper( line[ i ] ) == 'C' &&
+                char.ToUpper( line[ j ] ) == 'O' &&
+                char.ToUpper( line[ k ] ) == 'V' 
+            )
+            {
+                Console.WriteLine( "Veikur!" );
+                break;
+            }
+            else if( k == ( line.Length - 1 ) )
+            {
+                Console.WriteLine( "Ekki veikur!" );
+                break;
+            }
+            i++; j++; k++;
+        }
+    }
+}
